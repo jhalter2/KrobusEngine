ed9e7a80d3de5d0854d7c732ffc28cc559232a5b
jhalter2 <jehalter@gmail.com>
2019-04-22 21:33:18 -0500

    Update Collidable.cpp

--- KrobusEngine/CollisionSystem/Collidable.cpp
+++ KrobusEngine/CollisionSystem/Collidable.cpp
@@ -1,8 +1,10 @@
 #include "Collidable.h"
 #include "SceneManager.h"
 #include "Scene.h"
-#include "AzulCore.h"
 #include "RegistrationState.h"
+#include "CollisionVolumeAABB.h"
+#include "CollisionVolumeBSphere.h"
+#include "CollisionVolumeOBB.h"
 
 Collidable::Collidable() {
 	RegStateCurr = RegistrationState::CURRENTLY_DEREGISTERED;
@@ -37,4 +39,20 @@ void Collidable::SubmitCollisionDeregistration() {
 	assert(RegStateCurr == RegistrationState::CURRENTLY_REGISTERED);
 	SceneAttorney::SubmitCommand(SceneManager::GetCurrentScene(), pDeregistrationCmd);
 	RegStateCurr = RegistrationState::PENDING_DEREGISTRATION;
+}
+
+const CollisionVolume& Collidable::GetCollisionVolume() {
+	return *ColVolume;
+}
+
+void Collidable::SetColliderModel(Model* m , COLLISION_VOLUMES c) {
+	int i = (int)c;
+	pColModel = m;
+	if (i == 0)  ColVolume = new CollisionVolumeBSphere();
+	else if (i == 1) ColVolume = new CollisionVolumeAABB();
+	else ColVolume = new CollisionVolumeOBB();
+}
+
+void Collidable::UpdateCollisionData(Matrix& t, const float scale) {
+	ColVolume->ComputeData(pColModel, t, scale);
 }
