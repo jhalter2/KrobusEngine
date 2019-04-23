a07912e9f237e296a50913d9073a819745a2bd35
jhalter2 <jehalter@gmail.com>
2019-04-22 21:28:16 -0500

    Delete CollisionRegistrationCommand.cpp

--- KrobusEngine/CollisionSystem/CollisionRegistrationCommand.cpp
+++
@@ -1,9 +0,0 @@
-#include "CollisionRegistrationCommand.h"
-#include "Collidable.h"
-#include "CollidableAttorney.h"
-
-CollisionRegistrationCommand::CollisionRegistrationCommand(Collidable* up) : ptrUp(up) {}
-
-void CollisionRegistrationCommand::execute() {
-	CollidableAttorney::Registration::SceneRegistration(ptrUp);
-}
