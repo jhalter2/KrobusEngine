9dc18ccbab2f0a77fcef9e34adc793301dd8fb5b
jhalter2 <jehalter@gmail.com>
2019-04-22 21:27:55 -0500

    Update CollisionVolume.h
    collision volume now uses double dispatch pattern to handle disparate volumes

--- KrobusEngine/CollisionSystem/CollisionVolume.h
+++ KrobusEngine/CollisionSystem/CollisionVolume.h
@@ -3,18 +3,24 @@
 
 class Model;
 class Matrix;
+class Vect;
 class CollisionVolumeBSphere;
+class CollisionVolumeAABB;
+class CollisionVolumeOBB;
 
 class CollisionVolume {
 public:
-	CollisionVolume() {};
+	CollisionVolume() = default;
 	CollisionVolume(const CollisionVolume&) = delete;
 	CollisionVolume& operator=(const CollisionVolume&) = delete;
-	virtual ~CollisionVolume() {};
+	virtual ~CollisionVolume() = default;
 
-	virtual void ComputeData(Model*, Matrix&) {};
-	virtual bool Intersect(const CollisionVolume&) { return false;  };
-	virtual bool Intersect(const CollisionVolumeBSphere&) { return false;  };
+	virtual void ComputeData(Model*, Matrix&, const float scale) = 0;
+	virtual void DebugView(const Vect&) const = 0;
+	virtual bool IntersectAccept(const CollisionVolume& other) const = 0;
+	virtual bool IntersectVisit(const CollisionVolumeBSphere& other) const = 0;
+	virtual bool IntersectVisit(const CollisionVolumeAABB& other) const = 0;
+	virtual bool IntersectVisit(const CollisionVolumeOBB& other) const = 0;
 };
 
 #endif _CollisionVolume
