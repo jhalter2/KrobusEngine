a83bd08cc1d94a00848aecde14488f86b1691caa
jhalter2 <jehalter@gmail.com>
2019-07-09 13:25:15 -0500

    Update AlarmRegistrationCommand.cpp
    revised comments

--- KrobusEngine/AlarmSystem/AlarmRegistrationCommand.cpp
+++ KrobusEngine/AlarmSystem/AlarmRegistrationCommand.cpp
@@ -2,6 +2,10 @@
 #include "Alarmable.h"
 #include "AlarmableAttorney.h"
 
+//registration class for AlarmCommand system.
+//AlarmManager has list of commands to execute and are
+//processed when engine Update loop is called
+
 AlarmRegistrationCommand::AlarmRegistrationCommand(Alarmable* al, AlarmableManager::ALARM_ID id) : ptrAl(al), al_id(id), 
 	al_time(AlarmableAttorney::Registration::GetTime(al)) {}
 
