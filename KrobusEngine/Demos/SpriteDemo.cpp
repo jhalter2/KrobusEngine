3e159b7dbbaf493787b6bffd40849eb2705c3790
jhalter2 <jehalter@gmail.com>
2019-07-09 13:19:02 -0500

    Update SpriteDemo.cpp
    code change for sprite demo

--- KrobusEngine/Demos/SpriteDemo.cpp
+++ KrobusEngine/Demos/SpriteDemo.cpp
@@ -1,17 +1,11 @@
 #include "SpriteDemo.h"
-#include "WorldPlane.h"
-#include "AzulCore.h"
-#include "KeyStateTest.h"
-#include "KeyPressTester.h"
 #include "CameraManager.h"
-#include "FreezeTime.h"
 #include "BulletFactory.h"
 #include "CollisionManager.h"
 #include "FlyingCamera.h"
 #include "Player.h"
 #include "KrobusSprite.h"
 #include "SpriteTest.h"
-#include "SpriteFontTest.h"
 
 SpriteDemo::~SpriteDemo() {
 	SceneEnd();
