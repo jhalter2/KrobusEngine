50c02c9ef9ae2e811f26967f3190f61f787ff3c4
jhalter2 <jehalter@gmail.com>
2019-07-09 13:20:17 -0500

    Update SpriteDemo.cpp

--- KrobusEngine/Demos/SpriteDemo.cpp
+++ KrobusEngine/Demos/SpriteDemo.cpp
@@ -7,6 +7,11 @@
 #include "KrobusSprite.h"
 #include "SpriteTest.h"
 
+//demo shows off a basic use of the sprite system.
+//SpriteTest is the class that holds sprite content
+//for this specific test and will run it automatically when
+//this scene is loaded
+
 SpriteDemo::~SpriteDemo() {
 	SceneEnd();
 	DebugMsg::out("tank demo start deleted\n");
