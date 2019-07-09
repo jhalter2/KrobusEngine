2275701968dcfd217d28212cce4fc18399216ea2
jhalter2 <jehalter@gmail.com>
2019-07-09 13:24:03 -0500

    Update AlarmDeregistrationCommand.cpp
    added revised comments

--- KrobusEngine/AlarmSystem/AlarmDeregistrationCommand.cpp
+++ KrobusEngine/AlarmSystem/AlarmDeregistrationCommand.cpp
@@ -2,6 +2,9 @@
 #include "Alarmable.h"
 #include "AlarmableAttorney.h"
 
+//deregistration command for alarm command system.
+//processed through AlarmManager which is called during
+//engine Update Loop
 AlarmDeregistrationCommand::AlarmDeregistrationCommand(Alarmable* al, AlarmableManager::ALARM_ID id) : ptrAl(al), al_id(id) {}
 
 void AlarmDeregistrationCommand::Set(Alarmable* al, AlarmableManager::ALARM_ID id) {
