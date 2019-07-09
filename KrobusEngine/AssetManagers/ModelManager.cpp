bff9a6364f68b481b0fb57645af96f428e42303b
jhalter2 <jehalter@gmail.com>
2019-07-09 13:57:33 -0500

    Update ModelManager.cpp
    revised comments

--- KrobusEngine/AssetManagers/ModelManager.cpp
+++ KrobusEngine/AssetManagers/ModelManager.cpp
@@ -1,59 +1,70 @@
 #include "ModelManager.h"
+#include "EngineBridge.h"
+#include "d3dUtil.h"
+#include <assert.h>
+
+//ModelManager is a singleton where models
+//need to be loaded in before they are used.
+//Models are stored in a map by name(key)
+//All models to be used during runtime should be
+//loaded in using the LoadResources() function in the
+//KrobusEngine.cpp main class.
 
 ModelManager* ModelManager::ptrInstance = nullptr;
 
 typedef std::string string;
 
-const string ModelManager::DefaultPath = "Models/";
+//default models used in demos
+const string ModelManager::DefaultPath = "../Assets/Models/";
 const string ModelManager::Axis = "Axis.azul";
 const string ModelManager::Plane = "Plane.azul";
 const string ModelManager::Frigate = "space_frigate.azul";
 const string ModelManager::Cottage = "Cottage.azul";
-const string ModelManager::Player = "Tank.azul";
-const string ModelManager::Enemy = "Tank.azul";
+const string ModelManager::Tank = "Tank.azul";
+const string ModelManager::Missile = "rocket.azul";
+const string ModelManager::BoundingSphere = "BoundingSphere.azul";
+const string ModelManager::BoundingBox = "BoundingBox.azul";
 
 ModelManager::~ModelManager() {
-	//delete all models we created
+	//delete all models created
 	for (StorageMap::iterator it = storageMap.begin(); it != storageMap.end(); it++)
 	{
 		delete it->second;
 	}
-	DebugMsg::out("models deleted and now model manager deleted\n");
+	storageMap.clear();
 }
 
+//for loading models based on name(key) and filepath
 void ModelManager::privLoad(string name, string model) {
 	StorageMap::iterator it = storageMap.find(name);
 
 	if (it != storageMap.end()) {
-		DebugMsg::out("ERROR model Manager: model key '%s' already in use.\n'", name.c_str());
 		assert(false && "model key already used");
 	}
 	else {
+
 		string path = DefaultPath + model;
-		storageMap[name] = new Model(path.c_str());
-		DebugMsg::out("model Manager: model '%s' loaded.\n'", name.c_str());
+		storageMap[name] = new Model(EngineBridge::GetDevice(), path.c_str());
 	}
 }
 
 //for premade models
-void ModelManager::privLoad(string name, Model premade) {
+void ModelManager::privLoad(string name, Model::PreMadeModels premade) {
 	StorageMap::iterator it = storageMap.find(name);
 
 	if (it != storageMap.end()) {
-		DebugMsg::out("ERROR model Manager: model key '%s' already in use.\n'", name.c_str());
 		assert(false && "model key already used");
 	}
 	else {
-		storageMap[name] = new Model(premade);
-		DebugMsg::out("model Manager: model '%s' loaded.\n'", name.c_str());
+		storageMap[name] = new Model(EngineBridge::GetDevice(), premade , 1.0f);
 	}
 }
 
+//retrieve model by name(key)
 Model* ModelManager::privGet(string name) {
 	StorageMap::iterator it = storageMap.find(name);
 
 	if (it == storageMap.end()) {
-		DebugMsg::out("ERROR model Manager: Unknown model key '%s'.\n'", name.c_str());
 		assert(false && "Unknown model key");
 	}
 	return storageMap[name];
