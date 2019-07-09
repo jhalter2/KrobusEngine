11be16d03c074223cbd6c06cb7afc60aeb149a9b
jhalter2 <jehalter@gmail.com>
2019-07-09 13:23:04 -0500

    Update ALARM_ID.h
    added revised comments

--- KrobusEngine/AlarmSystem/ALARM_ID.h
+++ KrobusEngine/AlarmSystem/ALARM_ID.h
@@ -1,8 +1,12 @@
 #ifndef _ALARM_ID
 #define _ALARM_ID
 
+//enum class for the different alarms available
+//for each game object to use.
+//currently 3 alarms available to any 1 game object
+//(3 is arbitrary, just need some limit for
+//algorithm purposes later)
 enum class ALARM_ID {
-
 	ALARM_0,
 	ALARM_1,
 	ALARM_2 
