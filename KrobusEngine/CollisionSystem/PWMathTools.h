9e37c51c93c9abbc221a06091293838f01353c2e
jhalter2 <jehalter@gmail.com>
2019-07-10 18:51:35 -0500

    Update PWMathTools.h
    added some new comments and short description on the top

--- KrobusEngine/CollisionSystem/PWMathTools.h
+++ KrobusEngine/CollisionSystem/PWMathTools.h
@@ -1,3 +1,10 @@
+//static class that holds all the equations needed
+//by various functions throughout the engine. 
+//the math for every type of collision is contained here
+//and some function for different types of parameters.
+//too much to describe all up here, small descriptions
+//are paired with most equations
+
 #ifndef _PWMathTools
 #define _PWMathTools
 
@@ -5,8 +12,11 @@
 #include "CollisionVolumeBSphere.h"
 #include "CollisionVolumeAABB.h"
 #include "CollisionVolumeOBB.h"
+#include "Collidable.h"
 #include <cmath>
 #include <cfloat>
+#include <list>
+#include "Model.h"
 
 class PWMathTools {
 	//static class for housing math functions
@@ -239,7 +249,6 @@ public:
 
 	static void GetMaxWorldVect(Vect vects[], int count, Vect& newVect) {
 		//return maximum vect from array of vects
-		Vect maxV = vects[0];
 		float maxX = vects[0].X(), maxY = vects[0].Y(), maxZ = vects[0].Z();
 		for (int i = 1; i < count; i++) {
 			if (vects[i].X() > maxX) maxX = vects[i].X();
@@ -249,9 +258,19 @@ public:
 		newVect.set(maxX, maxY, maxZ);
 	}
 
+	static void GetMaxWorldVect(StandardVertex vects[], int count, Vect& newVect) {
+		//return maximum x,y,z from array of standardvertex
+		float maxX = vects[0].Pos.X(), maxY = vects[0].Pos.Y(), maxZ = vects[0].Pos.Z();
+		for (int i = 1; i < count; i++) {
+			if (vects[i].Pos.X() > maxX) maxX = vects[i].Pos.X();
+			if (vects[i].Pos.Y() > maxY) maxY = vects[i].Pos.Y();
+			if (vects[i].Pos.Z() > maxZ) maxZ = vects[i].Pos.Z();
+		}
+		newVect.set(maxX, maxY, maxZ);
+	}
+
 	static void GetMinWorldVect(Vect vects[], int count, Vect& newVect) {
 		//return minimum vect from array of vects
-		Vect minV = vects[0];
 		float minX = vects[0].X(), minY = vects[0].Y(), minZ = vects[0].Z();
 		for (int i = 1; i < count; i++) {
 			if (vects[i].X() < minX) minX = vects[i].X();
@@ -259,6 +278,142 @@ public:
 			if (vects[i].Z() < minZ) minZ = vects[i].Z();
 		}
 		newVect.set(minX, minY, minZ);
+	}
+
+	static void GetMinWorldVect(StandardVertex vects[], int count, Vect& newVect) {
+		//return minimum x,y,z from array of standard
+		float minX = vects[0].Pos.X(), minY = vects[0].Pos.Y(), minZ = vects[0].Pos.Z();
+		for (int i = 1; i < count; i++) {
+			if (vects[i].Pos.X() < minX) minX = vects[i].Pos.X();
+			if (vects[i].Pos.Y() < minY) minY = vects[i].Pos.Y();
+			if (vects[i].Pos.Z() < minZ) minZ = vects[i].Pos.Z();
+		}
+		newVect.set(minX, minY, minZ);
+	}
+
+	static float GetMaxFloat(float floats[], int count) {
+		//get max float in array of floats
+		float max = floats[0];
+		for (int i = 0; i < count; i++) {
+			if (floats[i] > max) max = floats[i];
+		}
+		return max;
+	}
+
+	static float GetMinFloat(float floats[], int count) {
+		//get min float in array of floats
+		float min = floats[0];
+		for (int i = 0; i < count; i++) {
+			if (floats[i] < min) min = floats[i];
+		}
+		return min;
+	}
+
+	static void GetMinMaxVectCollidable(std::list<Collidable*> objs, Vect& minVect, Vect& maxVect) {
+		//used by backend bsphere/aabb to get the max of the max and min of the min
+		//to create the group aabb
+		Vect min, max, tempMin, tempMax;
+		min = (*objs.begin())->GetBSphere().GetMin() + (*objs.begin())->GetBSphere().GetCenter();
+		max = (*objs.begin())->GetBSphere().GetMax() + (*objs.begin())->GetBSphere().GetCenter();
+		for (std::list<Collidable*>::iterator it = objs.begin(); it != objs.end(); ++it) {
+			//need the min for each axis
+			tempMin = (*it)->GetBSphere().GetMin() + (*it)->GetBSphere().GetCenter();
+			tempMax = (*it)->GetBSphere().GetMax() + (*it)->GetBSphere().GetCenter();
+			if (min.X() > tempMin.X()) min.X() = tempMin.X();
+			if (min.Y() > tempMin.Y()) min.Y() = tempMin.Y();
+			if (min.Z() > tempMin.Z()) min.Z() = tempMin.Z();
+			//need the max for each axis
+			if (max.X() < tempMax.X()) max.X() = tempMax.X();
+			if (max.Y() < tempMax.Y()) max.Y() = tempMax.Y();
+			if (max.Z() < tempMax.Z()) max.Z() = tempMax.Z();
+
+		}
+		minVect.set(min);
+		maxVect.set(max);
+	}
+
+	static bool PointInCell(const Vect& point, Vect& max, Vect& min, const Vect& maxPoint, const Vect& minPoint, const float scale) {
+		max.set(maxPoint);
+		max.Y() = 0.0f;
+		max.Z() *= scale;
+		min.set(minPoint);
+		min.Y() = 0.0f;
+		min.Z() *= scale;
+		return (PWMathTools::PointInInterval(point, max, min));
+	}
+
+	static int GetPosToCellInTerrainIndex(const Vect& point) {
+		//returns index of cell that a position is currently over
+		//(based on terrains using the default 256x256 cell count, starting at 0,0)
+		int xPos = (int)point.X(), zPos = (int)point.Z(), i = 0, j = 0;
+
+		if (xPos < 0 || zPos < 0) {
+			//return 0 if off the terrain to prevent crashing
+			return 0;
+		}
+		else {
+			//for every 18 units in pos, 1 cell is covered
+			i = xPos / 18;
+			j = zPos / 18;
+			//return index
+			return (i * 255) + j;
+		}
+	}
+
+	static float BarycentricCoordinate(const Vect& P, const Vect& A, const Vect& B, const Vect& C) {
+		//barycentric coordinates
+		//A = triangle[0], B = triangle[1], C = triangle[2], P = any point
+
+		//v0 = B - A
+		Vect v0 = B - A;
+		//v1 = C - B
+		Vect v1 = C - B;
+		//v2 = P - A
+		Vect v2 = P - A;
+
+		//a = v0 . v0
+		float a = v0.dot(v0);
+		//b = v1 . v0
+		float b = v1.dot(v0);
+		//c = v1 . v1
+		float c = v1.dot(v1);
+		//d = v2 . v0
+		float d = v2.dot(v0);
+		//e = v2 . v1
+		float e = v2.dot(v1);
+
+		//beta = (dc - be) / (ac-b^2)
+		float beta = ((d*c) - (b*e)) / ((a*c) - (b*b));
+		//gamma = (ae - db) / (ac-b^2)
+		float gamma = ((a*e) - (d*b)) / ((a*c) - (b*b));
+
+		//QA = A
+		float QA = A.Y();
+		//QB = B
+		float QB = B.Y();
+		//QC = C
+		float QC = C.Y();
+
+		//with the above, interpolate any quantity using the relation:
+		//QP = QA + beta(QB - QA) + gamma(QC - QB)
+		//Vect QP = QA + (beta * (QB - QA)) + (gamma * (QC - QB));
+		float QBA = QB - QA;
+		float QCB = QC - QB;
+		float QP = QA + (beta * (QBA)) + (gamma * (QCB));
+		return QP;
+	}
+
+	static bool InBottomTriangle(const Vect& P, const Vect& A) {
+		//returns true if point in top triangle of square
+		//or false if point in bottom triangle
+		Vect p1 = P;
+		p1.X() /= 6.0f;
+		p1.Z() /= 6.0f;
+		//v = P - A
+		Vect v = p1 - A;
+
+		//return v.X() > v.Z() which is true if in top triangle
+		return -v.X() > v.Z();
 	}
 };
 
