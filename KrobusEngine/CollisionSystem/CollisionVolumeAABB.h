7079fd3251f363c8405baf23770dab1d95ff777a
jhalter2 <jehalter@gmail.com>
2019-07-10 18:27:15 -0500

    Update CollisionVolumeAABB.h
    revised comments

--- KrobusEngine/CollisionSystem/CollisionVolumeAABB.h
+++ KrobusEngine/CollisionSystem/CollisionVolumeAABB.h
@@ -1,7 +1,6 @@
 #ifndef _CollisionVolumeAABB
 #define _CollisionVolumeAABB
 
-#include "AzulCore.h"
 #include "CollisionVolumeBoundingBoxBase.h"
 
 class CollisionVolumeBSphere;
@@ -12,14 +11,20 @@ public:
 	CollisionVolumeAABB() = default;
 	CollisionVolumeAABB(const CollisionVolumeAABB&) = delete;
 	CollisionVolumeAABB& operator=(const CollisionVolumeAABB&) = delete;
-	~CollisionVolumeAABB();
+	~CollisionVolumeAABB() = default;
 
 	void ComputeData(Model* mod, Matrix& mat, const float scale) override;
+	void ComputeData(const CollisionVolumeBSphere& other);
+	//the one below is used for terrain
+	void ComputeData(Vect& max, Vect& min, Matrix& mat, const float scale);
+
 	void DebugView(const Vect& col) const override;
 	bool IntersectAccept(const CollisionVolume& other) const override;
 	bool IntersectVisit(const CollisionVolumeBSphere& other) const override;
 	bool IntersectVisit(const CollisionVolumeAABB& other) const override;
 	bool IntersectVisit(const CollisionVolumeOBB& other) const override;
+	void SetMin(const Vect& newMin);
+	void SetMax(const Vect& newMax);
 };
 
 #endif _CollisionVolumeAABB
