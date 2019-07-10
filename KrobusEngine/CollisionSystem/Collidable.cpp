978d689ed1fe4ebbf47e2872ccfc79f7537329fd
jhalter2 <jehalter@gmail.com>
2019-07-10 17:21:18 -0500

    Update Collidable.cpp

--- KrobusEngine/CollisionSystem/Collidable.cpp
+++ KrobusEngine/CollisionSystem/Collidable.cpp
@@ -1,11 +1,3 @@
-#include "Collidable.h"
-#include "SceneManager.h"
-#include "Scene.h"
-#include "RegistrationState.h"
-#include "CollisionVolumeAABB.h"
-#include "CollisionVolumeBSphere.h"
-#include "CollisionVolumeOBB.h"
-
 //base class which GameObject class derives from.
 //collidable is the class which allows GameObjects
 //to register for the collision system.
@@ -14,16 +6,34 @@
 //of collision model available - 
 //bounding sphere, object oriented bounding box,
 //and axis aligned bounding box
+//
+//NOTE: all collidables have a default bounding sphere
+//that is in addition to their user chosen collision volume.
+//this bounding sphere is for optimization of collision 
+//calculations between groups of same-type objects
+
+#include "Collidable.h"
+#include "SceneManager.h"
+#include "Scene.h"
+#include "RegistrationState.h"
+#include "CollisionVolumeAABB.h"
+#include "CollisionVolumeBSphere.h"
+#include "CollisionVolumeOBB.h"
+#include <assert.h>
 
 Collidable::Collidable() {
 	RegStateCurr = RegistrationState::CURRENTLY_DEREGISTERED;
 	pRegistrationCmd = new CollisionRegistrationCommand(this);
 	pDeregistrationCmd = new CollisionDeregistrationCommand(this);
+	ColVolume = new CollisionVolumeBSphere();
+	pColBSphere = new CollisionVolumeBSphere();
 }
 
 Collidable::~Collidable() {
 	delete pRegistrationCmd;
 	delete pDeregistrationCmd;
+	delete ColVolume;
+	delete pColBSphere;
 }
 
 void Collidable::SceneRegistration() {
@@ -50,13 +60,20 @@ void Collidable::SubmitCollisionDeregistration() {
 	RegStateCurr = RegistrationState::PENDING_DEREGISTRATION;
 }
 
-//used by many internal methods for collision calculation
+//used by many internal methods during collision calculation
 const CollisionVolume& Collidable::GetCollisionVolume() {
 	return *ColVolume;
 }
 
-//user chooses collider model based on volume and model
+//gets the default bsphere of an object
+const CollisionVolumeBSphere& Collidable::GetBSphere() {
+	return *pColBSphere;
+}
+
+//user can set the CollisionVolume that will be used for a
+//specific object
 void Collidable::SetColliderModel(Model* m , COLLISION_VOLUMES c) {
+	delete ColVolume;
 	int i = (int)c;
 	pColModel = m;
 	if (i == 0)  ColVolume = new CollisionVolumeBSphere();
@@ -64,8 +81,9 @@ void Collidable::SetColliderModel(Model* m , COLLISION_VOLUMES c) {
 	else ColVolume = new CollisionVolumeOBB();
 }
 
-//automatically called to update collision data
-//on the backend
+//called by backend update to keep collision info
+//updated
 void Collidable::UpdateCollisionData(Matrix& t, const float scale) {
 	ColVolume->ComputeData(pColModel, t, scale);
+	pColBSphere->ComputeData(pColModel, t, scale);
 }
