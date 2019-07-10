f59a31d075f765c705db5861ead1281fc501352c
jhalter2 <jehalter@gmail.com>
2019-07-10 18:34:45 -0500

    Update CollisionVolumeBSphere.h
    revised comments

--- KrobusEngine/CollisionSystem/CollisionVolumeBSphere.h
+++ KrobusEngine/CollisionSystem/CollisionVolumeBSphere.h
@@ -1,15 +1,16 @@
 #ifndef _CollisionVolumeBSphere
 #define _CollisionVolumeBSphere
+
 #include "CollisionVolume.h"
 
 class CollisionVolumeAABB;
 class CollisionVolumeOBB;
 
-class CollisionVolumeBSphere : public CollisionVolume, public Align16 {
+class CollisionVolumeBSphere : public CollisionVolume {
 public:
 	CollisionVolumeBSphere() = default;
-	CollisionVolumeBSphere(const CollisionVolumeBSphere&) = delete;
-	CollisionVolumeBSphere& operator=(const CollisionVolumeBSphere&) = delete;
+	CollisionVolumeBSphere(const CollisionVolumeBSphere& other);
+	CollisionVolumeBSphere& operator=(const CollisionVolumeBSphere& other);
 	~CollisionVolumeBSphere();
 
 	void ComputeData(Model* mod, Matrix& mat, const float scale) override;
@@ -18,12 +19,20 @@ public:
 	bool IntersectVisit(const CollisionVolumeBSphere& other) const override;
 	bool IntersectVisit(const CollisionVolumeAABB& other) const override;
 	bool IntersectVisit(const CollisionVolumeOBB& other) const override;
+	const Matrix& GetWorld() const;
 	const Vect& GetCenter() const;
+	const Vect& GetMin() const;
+	const Vect& GetMax() const;
 	float GetRadius() const;
+	float GetScale() const;
 
 private:
+	Matrix world;
 	Vect Center;
+	Vect CornerMax;
+	Vect CornerMin;
 	float Radius;
+	float Scale;
 };
 
 #endif _CollisionVolumeBSphere
