ad38b2b08719f7b4513c90a67abe189033f6c4ee
jhalter2 <jehalter@gmail.com>
2019-07-10 18:45:06 -0500

    Update CollisionVolumeOBB.cpp
    revised comments

--- KrobusEngine/CollisionSystem/CollisionVolumeOBB.cpp
+++ KrobusEngine/CollisionSystem/CollisionVolumeOBB.cpp
@@ -1,16 +1,20 @@
+//collision volume oriented bounding box (OBB)
+//OBBs are only used when the user specifically selects
+//them while looking for a certain level of collision
+//precision. This means that they never need to be 
+//specially constructed, and only need their data
+//to be computed based on a model, world matrix, 
+//and a scale factor
+
 #include "CollisionVolumeAABB.h"
 #include "CollisionVolumeOBB.h"
 #include "CollisionVolumeBSphere.h"
 #include "PWMathTools.h"
 #include "Visualizer.h"
 
-CollisionVolumeOBB::~CollisionVolumeOBB() {
-	DebugMsg::out("collision volume obb deleted");
-}
-
 void CollisionVolumeOBB::ComputeData(Model* mod, Matrix& mat, const float scale) {
-	CornerMax = (scale * (mod->getMaxAABB()));
-	CornerMin = (scale * (mod->getMinAABB()));
+	CornerMax = (scale * (mod->GetMaxAABB()));
+	CornerMin = (scale * (mod->GetMinAABB()));
 	HalfDiagonal = 0.5f * (CornerMax - CornerMin);
 	CenterInWorld = (CornerMin + HalfDiagonal) * mat;
 	ScaleSquared = mat.get(ROW_2).magSqr();
@@ -18,8 +22,8 @@ void CollisionVolumeOBB::ComputeData(Model* mod, Matrix& mat, const float scale)
 	Scale = scale;
 }
 
-void CollisionVolumeOBB::DebugView(const Vect& col) const {
-	Visualizer::ShowOBB(*this, col);
+void CollisionVolumeOBB::DebugView(const Vect&) const {
+	//Visualizer::ShowOBB(*this, col);
 }
 
 bool CollisionVolumeOBB::IntersectAccept(const CollisionVolume& other) const {
