9c256df43f668cbf263f6cbf1b4723a3aaa49d12
jhalter2 <jehalter@gmail.com>
2019-07-10 19:25:17 -0500

    Update GameObject.cpp
    added more comments

--- KrobusEngine/GameObjectBase/GameObject.cpp
+++ KrobusEngine/GameObjectBase/GameObject.cpp
@@ -1,7 +1,19 @@
+//central class that objects in scenes will need to use.
+//derives from all types that gameobjects will need to be
+//such as updatable, drawable, etc. Contains registration.
+//
+//NOTE: contains functions to connect and disconnect an object
+//from a scene. Those two functions should be used when a specific
+//object needs to be removed from a scene with the intention of later
+//adding that object back into a scene. If there is no intention to
+//reuse an object, then it can simply be deregistered and deleted.
+
 #include "GameObject.h"
 #include "RegistrationState.h"
 #include "SceneManager.h"
 #include "Scene.h"
+#include "SceneAttorney.h"
+#include <assert.h>
 
 GameObject::GameObject() {
 	pRegCmd = new GameObjectEntryCommand(this);
