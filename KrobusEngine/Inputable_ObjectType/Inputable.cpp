d87df72757d51a427e0b3dbb0bc136f70e602ea6
jhalter2 <jehalter@gmail.com>
2019-07-10 19:43:50 -0500

    Update Inputable.cpp
    added comments

--- KrobusEngine/Inputable_ObjectType/Inputable.cpp
+++ KrobusEngine/Inputable_ObjectType/Inputable.cpp
@@ -1,3 +1,9 @@
+//base class for inputable objects that the GameObject class
+//derives from. Inputable allows objects to receive inputs
+//from the keyboard during runtime. The user must register 
+//objects for input based on specific keys to look for
+//and also for either a key press/key release event.
+
 #include "Inputable.h"
 #include "EVENT_TYPE.h"
 #include "RegistrationState.h"
@@ -6,6 +12,7 @@
 #include "InputRegistrationCommand.h"
 #include "InputDeregistrationCommand.h"
 #include "SceneAttorney.h"
+#include <assert.h>
 
 Inputable::Inputable() {
 	RegData.RegStateCurr = RegistrationState::CURRENTLY_DEREGISTERED;
@@ -13,11 +20,11 @@ Inputable::Inputable() {
 	RegData.pDeregistrationCmd = new InputDeregistrationCommand();
 }
 
-void Inputable::KeyPressed(AZUL_KEY k) {
+void Inputable::KeyPressed(char k) {
 	key = k;
 }
 
-void Inputable::KeyReleased(AZUL_KEY k) {
+void Inputable::KeyReleased(char k) {
 	key = k;
 }
 
@@ -25,23 +32,26 @@ Inputable::~Inputable() {
 	//delete commands
 	delete RegData.pRegistrationCmd;
 	delete RegData.pDeregistrationCmd;
-	DebugMsg::out("inputable deleted\n");
+	//DebugMsg::out("inputable deleted\n");
 }
 
-void Inputable::SceneRegistration(AZUL_KEY k, EVENT_TYPE e) {
-	assert(RegData.RegStateCurr == RegistrationState::PENDING_REGISTRATION);
+void Inputable::SceneRegistration(char k, EVENT_TYPE e) {
+	assert(RegData.RegStateCurr == RegistrationState::PENDING_REGISTRATION || 
+		RegData.RegStateCurr == RegistrationState::CURRENTLY_REGISTERED);
 	SceneAttorney::GetInputMgr()->Register(this, k, e);
 	RegData.RegStateCurr = RegistrationState::CURRENTLY_REGISTERED;
 }
 
-void Inputable::SceneDeregistration(AZUL_KEY k, EVENT_TYPE e) {
+void Inputable::SceneDeregistration(char k, EVENT_TYPE e) {
 	assert(RegData.RegStateCurr == RegistrationState::PENDING_DEREGISTRATION);
 	SceneAttorney::GetInputMgr()->Deregister(this, k, e);
 	RegData.RegStateCurr = RegistrationState::CURRENTLY_DEREGISTERED;
 }
 
-void Inputable::SubmitKeyRegistration(AZUL_KEY k, EVENT_TYPE e) {
-	assert(RegData.RegStateCurr == RegistrationState::CURRENTLY_DEREGISTERED);
+//event type must be specified press/release
+void Inputable::SubmitKeyRegistration(char k, EVENT_TYPE e) {
+	assert(RegData.RegStateCurr == RegistrationState::CURRENTLY_DEREGISTERED || 
+		RegData.RegStateCurr == RegistrationState::PENDING_REGISTRATION);
 	key = k;
 	ev = e;
 	RegData.pRegistrationCmd->Set(this, k, e);
@@ -49,7 +59,8 @@ void Inputable::SubmitKeyRegistration(AZUL_KEY k, EVENT_TYPE e) {
 	RegData.RegStateCurr = RegistrationState::PENDING_REGISTRATION;
 }
 
-void Inputable::SubmitKeyDeregistration(AZUL_KEY k, EVENT_TYPE e) {
+//event type must be specified press/release
+void Inputable::SubmitKeyDeregistration(char k, EVENT_TYPE e) {
 	assert(RegData.RegStateCurr == RegistrationState::CURRENTLY_REGISTERED);
 	key = k;
 	ev = e;
