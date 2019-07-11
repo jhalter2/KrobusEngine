8d19234e1dad0d1db121d050351ca10941f834f0
jhalter2 <jehalter@gmail.com>
2019-07-10 19:38:56 -0500

    Update SingleKeyEventManager.h
    updated class to directx version

--- KrobusEngine/InputSystem/SingleKeyEventManager.h
+++ KrobusEngine/InputSystem/SingleKeyEventManager.h
@@ -1,7 +1,6 @@
 #ifndef _SingleKeyEventManager
 #define _SingleKeyEventManager
 
-#include "AzulCore.h"
 #include <list>
 
 class Inputable;
@@ -12,19 +11,17 @@ private:
 	using StorageList = std::list<Inputable*>;
 	StorageList KeyPressCollection;
 	StorageList KeyReleaseCollection;
-	AZUL_KEY Key;
+	char Key;
 	bool pressed = false;
 public:
 	SingleKeyEventManager() = delete;
-	SingleKeyEventManager(AZUL_KEY k);
+	SingleKeyEventManager(char k);
 	SingleKeyEventManager(const SingleKeyEventManager&) = delete;
 	SingleKeyEventManager& operator= (const SingleKeyEventManager&) = delete;
 	~SingleKeyEventManager();
 
 	void ProcessKeyEvent();
 
-	//using StorageListRef = StorageList::iterator;
-	//StorageListRef ref;
 	void Register(Inputable* in, EVENT_TYPE e);
 	void Deregister(Inputable* in, EVENT_TYPE e);
 };
